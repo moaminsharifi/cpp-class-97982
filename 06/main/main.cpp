674b2605d45b647d9455f02838584d687fd0f81d
Amin Sharifi <bigm00bnd@gmail.com>
2019-04-20 22:16:51 +0430

    file added

---
+++ 06/main/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i =0;
+    while(true){
+
+
+        i++;
+        if(i == 9){
+            break;
+        }
+        cout<<i<<endl;
+
+    }
+    return 0;
+
+}
---
+++ Practices/Practice-01/JadvalZarb.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+int i,j;
+for(i=1;i<=9;i++){
+    for(j=1;j<=9;j++){
+    cout<<i*j<<"\t";
+    }
+
+    cout<<"\n";
+}
+    cout<<"\n\n\n"<<"press any key to exit...";
+return 0;
+}
+
